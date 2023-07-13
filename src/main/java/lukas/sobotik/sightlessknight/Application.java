8f963b6ccf2f0148be531a3403129c67e601150f
Pucky <osmset@gmail.com>
2023-07-13 19:07:57 +0200

    Vaadin Project

---
+++ src/main/java/lukas/sobotik/sightlessknight/Application.java
@@ -0,0 +1,24 @@
+package lukas.sobotik.sightlessknight;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "sightlessknight", variant = Lumo.DARK)
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
