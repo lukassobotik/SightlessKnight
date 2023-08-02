f7465ee94d39e9dbc63fbd146ed695041aa0cc72
PuckyEU <osmset@gmail.com>
2023-08-02 15:38:30 +0200

    All Test Positions are Checked

--- src/test/java/PerftTest.java
+++ src/test/java/PerftTest.java
@@ -3,25 +3,16 @@ import lukas.sobotik.sightlessknight.gamelogic.Board;
 import lukas.sobotik.sightlessknight.gamelogic.FenUtils;
 import lukas.sobotik.sightlessknight.gamelogic.GameState;
 import lukas.sobotik.sightlessknight.gamelogic.Piece;
-import lukas.sobotik.sightlessknight.gamelogic.entity.Team;
-import lukas.sobotik.sightlessknight.views.play.PlayView;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PerftTest {
-
-    private Board board;
-    private GameState gameState;
-    private PerftFunction perftFunction;
 
     @BeforeEach
     public void setUp() {
@@ -35,9 +26,9 @@ public class PerftTest {
         Piece[] pieces = new Piece[64];
         FenUtils fenUtils = new FenUtils(pieces);
         pieces = fenUtils.generatePositionFromFEN(fen);
-        board = new Board(64, pieces, fenUtils);
-        gameState = new GameState(board, fenUtils.getStartingTeam(), false);
-        perftFunction = new PerftFunction(board, gameState, null);
+        Board board = new Board(64, pieces, fenUtils);
+        GameState gameState = new GameState(board, fenUtils.getStartingTeam(), false);
+        PerftFunction perftFunction = new PerftFunction(board, gameState, null);
 
         int actualPositions = perftFunction.playMoves(depth, fenUtils.getStartingTeam(), false);
         assertEquals(expectedPositions, actualPositions, "FEN: " + fen + ", Team " + fenUtils.getStartingTeam() + ", Depth " + depth + " positions mismatch.");
@@ -55,19 +46,19 @@ public class PerftTest {
                 Arguments.of(enPassantWhite, 3, 100),
                 Arguments.of(enPassantWhite, 4, 485),
                 Arguments.of(enPassantWhite, 5, 3567),
-//                Arguments.of(enPassantWhite, 6, 21270),
+                Arguments.of(enPassantWhite, 6, 21270),
 
                 Arguments.of(enPassantBlack, 1, 3),
                 Arguments.of(enPassantBlack, 2, 14),
                 Arguments.of(enPassantBlack, 3, 53),
                 Arguments.of(enPassantBlack, 4, 328),
                 Arguments.of(enPassantBlack, 5, 1747),
-//                Arguments.of(enPassantBlack, 6, 12036),
+                Arguments.of(enPassantBlack, 6, 12036),
 
                 Arguments.of(castling, 1, 26),
-//                Arguments.of(castling, 2, 568),
-//                Arguments.of(castling, 3, 13744),
-//                Arguments.of(castling, 4, 314346),
+                Arguments.of(castling, 2, 568),
+                Arguments.of(castling, 3, 13744),
+                Arguments.of(castling, 4, 314346),
 //                Arguments.of(castling, 5, 7594526),
 //                Arguments.of(castling, 6, 179862938),
 
