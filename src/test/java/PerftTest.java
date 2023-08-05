4a9edb481e77fcdfd09d37de39059c14764caef4
PuckyEU <osmset@gmail.com>
2023-08-05 10:42:30 +0200

    Even More Test Positions

--- src/test/java/PerftTest.java
+++ src/test/java/PerftTest.java
@@ -31,7 +31,10 @@ public class PerftTest {
         PerftFunction perftFunction = new PerftFunction(board, gameState, null);
 
         int actualPositions = perftFunction.playMoves(depth, fenUtils.getStartingTeam(), false);
-        assertEquals(expectedPositions, actualPositions, "FEN: " + fen + ", Team " + fenUtils.getStartingTeam() + ", Depth " + depth + " positions mismatch.");
+        assertEquals(expectedPositions, actualPositions, "FEN: " + fen
+                + ", Move " + GameState.moveNumber
+                + ", Pieces Captured " + gameState.capturedPieces
+                + ", Depth " + depth + " positions mismatch.");
     }
 
     private static Stream<Arguments> fenPositionsProvider() {
@@ -39,6 +42,11 @@ public class PerftTest {
         var enPassantBlack = "K1k5/8/8/8/2p5/8/1P6/8 w - - 0 1";
         var castling = "r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1";
         var startingPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        var testPos2 = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
+        var testPos3 = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ";
+        var testPos4 = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
+        var testPos5 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+        var testPos6 = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
         // Add FENs and their corresponding expected positions for each depth
         return Stream.of(
                 Arguments.of(enPassantWhite, 1, 5),
@@ -65,7 +73,43 @@ public class PerftTest {
                 Arguments.of(startingPosition, 1, 20),
                 Arguments.of(startingPosition, 2, 400),
                 Arguments.of(startingPosition, 3, 8902),
-                Arguments.of(startingPosition, 4, 197281)
+                Arguments.of(startingPosition, 4, 197281),
+//                Arguments.of(startingPosition, 5, 4865609),
+//                Arguments.of(startingPosition, 6, 119060324),
+
+                Arguments.of(testPos2, 1, 48),
+                Arguments.of(testPos2, 2, 2039),
+                Arguments.of(testPos2, 3, 97862),
+                Arguments.of(testPos2, 4, 4085603),
+//                Arguments.of(testPos2, 5, 193690690),
+//                Arguments.of(testPos2, 6, 8031647685),
+
+                Arguments.of(testPos3, 1, 14),
+                Arguments.of(testPos3, 2, 191),
+                Arguments.of(testPos3, 3, 2812),
+                Arguments.of(testPos3, 4, 43238),
+//                Arguments.of(testPos3, 5, 674624),
+//                Arguments.of(testPos3, 6, 11030083),
+
+                Arguments.of(testPos4, 1, 6),
+                Arguments.of(testPos4, 2, 264),
+                Arguments.of(testPos4, 3, 9467),
+                Arguments.of(testPos4, 4, 422333),
+//                Arguments.of(testPos4, 5, 15833292),
+//                Arguments.of(testPos4, 6, 706045033),
+
+                Arguments.of(testPos5, 1, 44),
+                Arguments.of(testPos5, 2, 1486),
+                Arguments.of(testPos5, 3, 62379),
+                Arguments.of(testPos5, 4, 2103487),
+//                Arguments.of(testPos5, 5, 89941194),
+
+                Arguments.of(testPos6, 1, 46),
+                Arguments.of(testPos6, 2, 2079),
+                Arguments.of(testPos6, 3, 89890),
+                Arguments.of(testPos6, 4, 3894594)
+//                Arguments.of(testPos6, 5, 164075551),
+//                Arguments.of(testPos6, 6, 6923051137)
         );
     }
 }
