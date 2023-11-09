fa9dee49c96f89b6a14381ee96c93336baba24a3
lukassobotik <osmset@gmail.com>
2023-11-09 21:24:10 +0100

    Removed unused Code

--- src/main/java/lukas/sobotik/sightlessknight/views/HomeView.java
+++ src/main/java/lukas/sobotik/sightlessknight/views/HomeView.java
@@ -1,9 +1,7 @@
 package lukas.sobotik.sightlessknight.views;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -20,41 +18,5 @@ public class HomeView extends VerticalLayout {
         });
 
         add(practiceMoves, knightButton);
-
-
-        HorizontalLayout containerLayout = new HorizontalLayout();
-        containerLayout.setSizeFull();
-        containerLayout.addClassName("column-container");
-
-        Div leftColumn = new Div();
-        Div middleColumn = new Div();
-        Div rightColumn = new Div();
-        leftColumn.addClassName("column");
-        leftColumn.addClassName("left-column");
-        middleColumn.addClassName("column");
-        middleColumn.addClassName("middle-column");
-        rightColumn.addClassName("column");
-        rightColumn.addClassName("right-column");
-
-        middleColumn.getStyle().set("resize", "horizontal");
-        middleColumn.getStyle().set("overflow", "auto");
-
-        String resizeObserverScript = "if (window.ResizeObserver) { " +
-                "var resizeTimeout; " +
-                "var observer = new ResizeObserver(function(entries) { " +
-                "  clearTimeout(resizeTimeout); " +
-                "  resizeTimeout = setTimeout(function() { " +
-                "    entries.forEach(function(entry) { " +
-                "      var width = entry.contentRect.width; " +
-                "      entry.target.style.height = width + 'px'; " +
-                "    }); " +
-                "  }, 250); " + // Adjust the delay (in milliseconds) as needed
-                "}); " +
-                "observer.observe(this); " +
-                "}";
-        middleColumn.getElement().executeJs(resizeObserverScript);
-
-        containerLayout.add(leftColumn, middleColumn, rightColumn);
-        add(containerLayout);
     }
 }
