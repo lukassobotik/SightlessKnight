55d1697b4b44229fa04f8b1fbd1d54816ef3e448
lukassobotik <osmset@gmail.com>
2023-11-19 17:58:15 +0100

    GameState.java Documentation

--- src/main/java/lukas/sobotik/sightlessknight/gamelogic/GameState.java
+++ src/main/java/lukas/sobotik/sightlessknight/gamelogic/GameState.java
@@ -12,18 +12,30 @@ public class GameState {
     @Getter
     public Board board;
     public static Team currentTurn;
-    public boolean hasGameEnded = false;
     List<Move> validMoves;
     BoardLocation selectedPieceLocation;
+
     public static int moveNumber = 0, enPassantCaptures = 0;
     static final Team playerTeam = Team.WHITE;
+
     public static boolean isPawnPromotionPending = false;
     public static BoardLocation promotionLocation;
     static PieceType selectedPromotionPieceType;
+
+    public boolean hasGameEnded = false;
     public static boolean kinglessGame = false;
+
     public List<Move> moveHistory;
     public List<String> parsedMoveHistory;
     public List<String> fenMoveHistory;
+
+    /**
+     * Constructs a new GameState object.
+     *
+     * @param board the game board
+     * @param startingTeam the team that starts the game
+     * @param kinglessGame a boolean indicating if the game is a kingless game
+     */
     public GameState(Board board, Team startingTeam, boolean kinglessGame) {
         validMoves = new ArrayList<>();
         moveHistory = new ArrayList<>();
@@ -35,12 +47,27 @@ public class GameState {
         this.board = board;
     }
 
+    /**
+     * Moves the selected piece to the given destination and ends the current turn.
+     * If the destination is not null, the selected piece is moved to the destination on the game board.
+     * The current turn is then switched to the opposing team.
+     *
+     * @param destination the location on the board where the selected piece will be moved
+     */
     public void movePieceAndEndTurn(BoardLocation destination) {
         if (destination != null) {
             board.movePiece(new Move(selectedPieceLocation, destination));
         }
         currentTurn = (currentTurn == Team.WHITE) ? Team.BLACK : Team.WHITE;
     }
+
+    /**
+     * Moves the selected piece to the given destination and ends the current turn.
+     * If the move object has both the 'from' and 'to' locations set, the selected piece is moved from the 'from' location to the 'to' location on the game board.
+     * The current turn is then switched to the opposing team.
+     *
+     * @param move the Move object containing the 'from' and 'to' locations
+     */
     public void movePieceAndEndTurn(Move move) {
         if (move.getFrom() != null && move.getTo() != null) {
             board.movePiece(move);
@@ -100,6 +127,12 @@ public class GameState {
         validMoves.clear();
         selectedPieceLocation = null;
     }
+
+    /**
+     * Updates the move flags and pieces of the given move based on the valid move.
+     * @param move the move to be updated
+     * @param validMove the valid move to update the move with
+     */
     private void updateMoveFlagsAndPieces(Move move, Move validMove) {
         if (validMove.getMoveFlag() != null && !validMove.getMoveFlag().equals(MoveFlag.none)) {
             move.setMoveFlag(validMove.getMoveFlag());
@@ -111,6 +144,13 @@ public class GameState {
             move.setMovedPiece(validMove.getMovedPiece());
         }
     }
+
+    /**
+     * Determines if the given move is an en passant capture.
+     * @param move the move to check
+     * @param validMove the valid move
+     * @return true if the move is an en passant capture, false otherwise
+     */
     private boolean isEnPassantCapture(Move move, Move validMove) {
         BoardLocation enPassantCapture = new BoardLocation(move.getTo().getX(), move.getFrom().getY());
         return board.getPiece(enPassantCapture) != null
@@ -120,12 +160,27 @@ public class GameState {
                 && move.getFrom().getX() != move.getTo().getX()
                 && (board.getPiece(enPassantCapture).doublePawnMoveOnMoveNumber == GameState.moveNumber - 1);
     }
+
+    /**
+     * Determines if the given move is a pawn promotion.
+     *
+     * @param move the move to check
+     * @param validMove the valid move
+     * @return true if the move is a pawn promotion, false otherwise
+     */
     private boolean isPawnPromotion(Move move, Move validMove) {
         return (move.getTo().getY() == 0 || move.getTo().getY() == 7)
                 && move.getMovedPiece().type == PieceType.PAWN
                 && move.getPromotionPiece() != null;
     }
+
     public static int capturedPieces = 0, enPassantCapturesReturned = 0;
+
+    /**
+     * Undoes a move and reverts the game state to the previous state.
+     *
+     * @param move the move to undo
+     */
     public void undoMove(Move move) {
         if (moveNumber - 1 >= 0) moveNumber -= 1;
 
@@ -133,6 +188,12 @@ public class GameState {
 
         currentTurn = (currentTurn == Team.WHITE) ? Team.BLACK : Team.WHITE;
     }
+
+    /**
+     * Creates a parsed move history for the given move.
+     *
+     * @param move the move to create the parsed move history for
+     */
     public void createParsedMoveHistory(Move move) {
         boolean isPromotionSquare = (move.getTo().getY() == 0 && move.getMovedPiece().team == Team.BLACK) || (move.getTo().getY() == 7 && move.getMovedPiece().team == Team.WHITE);
         if (move.getMovedPiece().type.equals(PieceType.PAWN) && isPromotionSquare && move.getMovedPiece().promotion == null) return;
@@ -144,11 +205,21 @@ public class GameState {
         FenUtils fenUtils = new FenUtils(board.pieces, board.whiteKingLocation, board.blackKingLocation, board.lastToLocation, board.lastDoublePawnMoveWithWhitePieces, board.lastDoublePawnMoveWithBlackPieces);
         fenMoveHistory.add(fenUtils.generateFenFromPosition(fenUtils.pieces));
     }
+
+    /**
+     * Resets the move history, parsed move history, and fen move history.
+     */
     public void resetHistory() {
         moveHistory = new ArrayList<>();
         parsedMoveHistory = new ArrayList<>();
         fenMoveHistory = new ArrayList<>();
     }
+
+    /**
+     * Promotes a pawn on the board to a selected piece type.
+     *
+     * @param selectedPieceType the selected piece type to promote the pawn to
+     */
     public void promotePawn(PieceType selectedPieceType) {
         board.promotePawn(promotionLocation, selectedPieceType);
         currentTurn = (currentTurn == Team.WHITE) ? Team.BLACK : Team.WHITE;
