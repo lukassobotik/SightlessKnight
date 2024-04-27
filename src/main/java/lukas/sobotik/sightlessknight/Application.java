32a6f56ba7aca4fb49ec7ff9dd5579fd9a14dc1f
lukassobotik <osmset@gmail.com>
2024-04-27 15:33:45 +0200

    Google Cloud Hosting

--- src/main/java/lukas/sobotik/sightlessknight/Application.java
+++ src/main/java/lukas/sobotik/sightlessknight/Application.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The entry point of the Spring Boot application.
@@ -17,7 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @PWA(name = "SightlessKnight", shortName = "SightKn", manifestPath = "manifest.json")
 @Theme(value = "sightlessknight", variant = Lumo.DARK)
-public class Application implements AppShellConfigurator {
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
